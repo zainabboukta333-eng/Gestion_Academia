536eb8de7eb7c10015a0081e3714e0332bac34e4
Zainab Boukta El Bourhari <zainab.boukta.el.bourhari@alu.ceacfp.es>
2026-02-10 09:54:31 +0100

    initial commit feature persona

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -1,7 +1,44 @@
 package org.example;
 
 public class Persona {
-    private String name;
-    private String surname;
-    private int age;
+    //Atributos
 }
+    private int id;
+    private String nombre;
+    private String email;
+
+// Constructor con atributos
+    public Persona(int id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+    // Constructor sin id
+public Persona(String nombre, String email) {
+    this.nombre = nombre;
+    this.email = email;
+}
+// MÉTODOS: getter, setter
+
+public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+public String getNombre() {
+        return nombre;
+}
+public void setnombre(String nombre) {
+        this.nombre = nombre;
+}
+public String getEmail() {
+        return email;
+}
+public void setEmail(String email) {
+        this.email = email;
+}
+public void
+
+
